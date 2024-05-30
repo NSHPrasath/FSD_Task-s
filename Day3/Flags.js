fe14e992177b77eb8842188d141a4787c32384c8
NSHPrasath <ns.h.prasath301@gmail.com>
2024-05-30 17:14:30 +0530

    add all

---
+++ Day3/Flags.js
@@ -0,0 +1,7 @@
+//  "flags": {
+//       "png": "https://flagcdn.com/w320/md.png",
+//       "svg": "https://flagcdn.com/md.svg",
+//       "alt": "The flag of Moldova is composed of three equal vertical bands of blue, yellow and red, with the national coat of arms centered in the yellow band."
+
+let flags = "https://flagcdn.com/w320/md.png";
+console.log(flags);
